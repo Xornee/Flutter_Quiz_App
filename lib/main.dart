6e4df012ee261315faf754e28afb16fb3c770026
xorne <xorne@protonmail.ch>
2022-05-17 10:51:30 +0200

    Section2 - 51

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,62 @@
 import 'package:flutter/material.dart';
+import './question.dart';
+import './answer.dart';
 
-// void main() {
-//   runApp(MyApp());
-// }
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => MyAppState();
+  State<StatefulWidget> createState() => _MyAppState();
 }
 
-class MyAppState extends State<MyApp> {
-  int questionIndex = 0;
+final questions = const [
+  {
+    'questionText': 'What\'s the best JS Framewor/Library',
+    'answers': ['Vue', 'React', 'Angular']
+  },
+  {
+    'questionText': 'What\'s the best IDE',
+    'answers': ['VSC', 'WebStorm', 'Atom']
+  },
+  {
+    'questionText': 'Why lemons are awesome? :D',
+    'answers': [
+      'Coz they are tasty',
+      'Coz they are yellow',
+      'Coz you can make lemonade :D'
+    ]
+  },
+];
 
-  void answerQuestions() {
+class _MyAppState extends State<MyApp> {
+  int _questionIndex = 0;
+
+  void _answerQuestions() {
     setState(() {
-      questionIndex++;
+      _questionIndex++;
     });
-    print(questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What\'s the best language',
-      'What\'s the best IDE',
-      'Why lemons are awesome? :D'
-    ];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("My First App"),
-        ),
-        body: Column(
-          children: [
-            Text(
-              questions[questionIndex],
-            ),
-            ElevatedButton(
-              onPressed: answerQuestions,
-              child: Text('React'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                print('answer Choosen');
-              },
-              child: Text('Vue'),
-            ),
-            ElevatedButton(
-              onPressed: () => print('answer Choosen'),
-              child: Text('Angular'),
-            ),
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: Text("My First App"),
+          ),
+          body: _questionIndex < questions.length
+              ? Column(
+                  children: [
+                    Question(
+                      questions[_questionIndex]['questionText'] as String,
+                    ),
+                    ...(questions[_questionIndex]['answers'] as List<String>)
+                        .map((answer) {
+                      return Answer(_answerQuestions, answer);
+                    }).toList()
+                  ],
+                )
+              : Center(child: Text('Nice :D'))),
     );
   }
 }
